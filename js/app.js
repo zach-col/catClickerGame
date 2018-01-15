4478020055ab070d6c94cde8e5f09eef4febaba4
zach-col <theperfectpicture100@gmail.com>
2018-01-15 14:03:17 -0600

    feat: add nicknames function and show data in view

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ var ViewModel = function() {
     this.name = ko.observable('Billy');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('https://www.biography.com/.image/t_share/MTE5NDg0MDU0OTU2OTAxOTAz/albert-einstein-9285408-1-402.jpg');
+    this.nicknames = ko.observableArray(['Bill','Billy Bob', 'Baller']);
     this.incrementCounter = function() {
         this.clickCount(this.clickCount() + 1);
     };
