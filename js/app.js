09392021e6543191e65349ce35e4dac750a7864f
zach-col <theperfectpicture100@gmail.com>
2018-01-20 00:02:31 +0000

    refactor: move model to cat function

--- js/app.js
+++ js/app.js
@@ -1,12 +1,10 @@
-var ViewModel = function() {
+var Cat = function() {
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Billy');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('https://www.biography.com/.image/t_share/MTE5NDg0MDU0OTU2OTAxOTAz/albert-einstein-9285408-1-402.jpg');
     this.nicknames = ko.observableArray(['Bill','Billy Bob', 'Baller']);
-    this.incrementCounter = function() {
-        this.clickCount(this.clickCount() + 1);
-    };
+
 
     this.title = ko.computed(function(){
         var title;
@@ -26,6 +24,17 @@ var ViewModel = function() {
         }
         return title;
     }, this);
+}
+
+
+
+var ViewModel = function() {
+
+    this.currentCat = ko.observable( new Cat() );
+
+    this.incrementCounter = function() {
+        this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+    };
 
 }
 
