8d01f55d00a5ef7ef712e822cdc7dd7714792de7
zach-col <theperfectpicture100@gmail.com>
2018-01-20 01:50:42 +0000

    refactor: cat function to take input data

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
-var Cat = function() {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Billy');
-    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-    this.imgAttribution = ko.observable('https://www.biography.com/.image/t_share/MTE5NDg0MDU0OTU2OTAxOTAz/albert-einstein-9285408-1-402.jpg');
-    this.nicknames = ko.observableArray(['Bill','Billy Bob', 'Baller']);
-
+var Cat = function(data) {
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.imgAttribution = ko.observable(data.imgAttribution);
+    this.nicknames = ko.observableArray(data.nicknames);
 
     this.title = ko.computed(function(){
         var title;
@@ -31,7 +30,14 @@ var Cat = function() {
 var ViewModel = function() {
     var self = this;
 
-    this.currentCat = ko.observable( new Cat() );
+    this.currentCat = ko.observable( new Cat({
+        clickCount: 0,
+        name: 'Billy',
+        imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+        imgAttribution: 'https://www.biography.com/.image/t_share/MTE5NDg0MDU0OTU2OTAxOTAz/albert-einstein-9285408-1-402.jpg',
+        nicknames: ['Bill','Billy Bob', 'Baller']
+
+    }) );
 
     this.incrementCounter = function() {
         self.currentCat().clickCount(self.currentCat().clickCount() + 1);
