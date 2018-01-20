f72135a7da711caaf982acb594221a1c5d7fb9aa
zach-col <theperfectpicture100@gmail.com>
2018-01-20 02:13:22 +0000

    feat: show currently clicke cat

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
+var initialCats = [
+    {
+        clickCount: 0,
+        name: 'Bob',
+        imgSrc: 'img/4154543904_6e2428c421_z.jpg',
+        imgAttribution: 'https://www.biography.com/.image/t_share/MTE5NDg0MDU0OTU2OTAxOTAz/albert-einstein-9285408-1-402.jpg',
+        nicknames: ['Bob','Bobby', 'Bobby the barber']
+    },
+    {
+        clickCount: 0,
+        name: 'Joe',
+        imgSrc: 'img/22252709_010df3379e_z.jpg',
+        imgAttribution: 'http://www.success.com/sites/default/files/main/articles/nightingale2_0.jpg',
+        nicknames: ['Joe','Joe smoe', 'Joe flow']
+    },
+]
+
 var Cat = function(data) {
     this.clickCount = ko.observable(data.clickCount);
     this.name = ko.observable(data.name);
@@ -30,18 +47,21 @@ var Cat = function(data) {
 var ViewModel = function() {
     var self = this;
 
-    this.currentCat = ko.observable( new Cat({
-        clickCount: 0,
-        name: 'Billy',
-        imgSrc: 'img/434164568_fea0ad4013_z.jpg',
-        imgAttribution: 'https://www.biography.com/.image/t_share/MTE5NDg0MDU0OTU2OTAxOTAz/albert-einstein-9285408-1-402.jpg',
-        nicknames: ['Bill','Billy Bob', 'Baller']
+    this.catList = ko.observableArray([]);
 
-    }) );
+    initialCats.forEach(function(catItem){
+        self.catList.push( new Cat(catItem) );
+    });
+
+    this.currentCat = ko.observable( this.catList()[0]) ;
 
     this.incrementCounter = function() {
         self.currentCat().clickCount(self.currentCat().clickCount() + 1);
     };
+
+    this.setCat = function(clickedCat) {
+        self.currentCat(clickedCat);
+    }
 
 }
 
